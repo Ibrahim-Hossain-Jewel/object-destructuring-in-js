de80c5f8f65590f4c5bb326495bbf27e908e3866
ibrahim hossain <ibrahimhossaincse@gmail.com>
2021-08-10 19:55:54 +0600

    made a function that accept object as an argument

--- objectDestructuring.js
+++ objectDestructuring.js
@@ -38,3 +38,32 @@ const student = {
 }
 //Now destructuring the properties with new destructuring name for super descriptive 
 const {q:sname,w:sage,e:sheight,r} = student;//Assign new variable for super descriptive here, sname is new name along with sage is new identifier sheight also a new identifier but  here w and r is default destrcturing extract value
+console.log(sname,sage,sheight,sheight);
+
+const myobj4 = {
+    uuid : 1212121,
+    userName : 'es6 jewel',
+    loggedin : new Date()
+}
+//now destructuring with assign new variable for super descriptive.
+const {uuid : userId, userName : name, loggedin: lastLoginDate} = myobj4;
+console.log(lastLoginDate.toLocaleString());    
+
+//set default value with object destructuring
+const myobj5 = {
+    a1 : 12,
+    b1 : 13,
+    c1 : 14,
+    d1 : 15
+};
+//Now destructure myobj5 with default value
+const {a1,b1,c1,d1,e1="this value is absent"} = myobj5;
+console.log(e1);
+//set default value as function argument.
+function stateUser({user = "bob", memberType}) {
+    console.log(`username ${user} and memberType is ${memberType}`);
+}
+//Pass an object to the function as argument in js.
+stateUser({user: 'ibrahim hossain', memberType : 'premium'});
+
+
